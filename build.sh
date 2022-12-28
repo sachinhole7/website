9f753e6df694c0f739c674ad292b6d126f47d526
Sachin Adinath Hole <97550790+sachinhole7@users.noreply.github.com>
2022-12-28 13:15:39 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,3 +8,10 @@ whoami
 echo "# This is my Blog." >> README.md
 
 cat README.md
+
+git config --global user.name "sachinhole7"
+git config --global user.email "sachinhole.xyz@gmail.com"
+
+git add -A
+git commit -am "build action"
+git push
